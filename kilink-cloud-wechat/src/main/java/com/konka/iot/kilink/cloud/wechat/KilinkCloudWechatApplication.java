5dc4314d1c59023f2933a26f698d382cf6ecaa48
xiexinyuan <xiexinyuan@konka.com>
2020-03-10 10:15:28 +0800

    初始化导入微信小程序后台接口服务

---
+++ kilink-cloud-wechat/src/main/java/com/konka/iot/kilink/cloud/wechat/KilinkCloudWechatApplication.java
@@ -0,0 +1,13 @@
+package com.konka.iot.kilink.cloud.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KilinkCloudWechatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KilinkCloudWechatApplication.class, args);
+    }
+
+}
---
+++ kilink-cloud-wechat/src/test/java/com/konka/iot/kilink/cloud/wechat/KilinkCloudWechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konka.iot.kilink.cloud.wechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KilinkCloudWechatApplicationTests {
+
+    @Test
+    void contextLoads( ) {
+    }
+
+}
